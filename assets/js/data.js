4a6b2631ec5121259ab0bae704a86e9f97020cd9
jocoso <joshua.collado022@gmail.com>
2024-05-22 20:04:27 -0400

    Created a function to easily retrieve any data needed.

---
+++ assets/js/data.js
@@ -0,0 +1,13 @@
+/**
+ * Retrieves the data from the Server API
+ * and returns it as an Object
+ * 
+ * @param {String} url the API used to fetch request 
+ * @return {Object} data requested
+ */
+const getAPIData = (url) => {
+    
+    const output = {};
+    return output;
+    
+}
