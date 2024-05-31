69bff649cd3a9f2ab8c57a7067aca5ec86c5a30e
jocoso <joshua.collado022@gmail.com>
2024-05-30 20:01:36 -0400

    Modified file logic.js to make it work with data.js functions

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,22 +1,27 @@
-import { getTrack } from './data.js';
-
 async function displaySongInfo(songName, artistName) {
     try {
-        const track = await getTrack(songName, artistName, apiKey);
+        const track = getTrack(songName, artistName, apiKey);
 
-        if (track) {
-            const lyricsUrl = `https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${track.track_id}&apikey=${apiKey}`;
-            const lyricsResponse = await $.getJSON(lyricsUrl);
-            const lyrics = lyricsResponse.message.body.lyrics ? lyricsResponse.message.body.lyrics.lyrics_body : 'Lyrics not found';
+        // if (track) {
+        //     const lyricsUrl = `https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${track.track_id}&apikey=${apiKey}`;
+        //     const lyricsResponse = await $.getJSON(lyricsUrl);
+        //     const lyrics = lyricsResponse.message.body.lyrics ? lyricsResponse.message.body.lyrics.lyrics_body : 'Lyrics not found';
 
-            $('#container-songname').text(songName);
-            $('#container-artistname').text(artistName);
-            $('#container-albumcover').attr('src', track.album_coverart_350x350 || '');
-            $('#container-albumname').text(track.album_name || 'Unknown Album');
-            $('#container-lyrics').text(lyrics);
-        } else {
-            $('#lyrics').text('Track not found');
-        }
+        //     $('#container-songname').text(songName);
+        //     $('#container-artistname').text(artistName);
+        //     $('#container-albumcover').attr('src', track.album_coverart_350x350 || '');
+        //     $('#container-albumname').text(track.album_name || 'Unknown Album');
+        //     $('#container-lyrics').text(lyrics);
+        
+        // } else {
+        //     $('#lyrics').text('Track not found');
+        // }
+
+        track.then((data) => {
+            console.log(data);
+        }).catch((error) => {
+            console.log(error);
+        })
     } catch (error) {
         console.error('Error displaying song info:', error);
         $('#lyrics').text('Error fetching data');
