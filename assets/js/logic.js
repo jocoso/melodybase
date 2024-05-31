94332d556029006dd09229c1baba8c15ae5ea440
jocoso <joshua.collado022@gmail.com>
2024-05-30 20:48:34 -0400

    Fixed collisions between data.js and logic.js

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,6 @@
 async function displaySongInfo(songName, artistName) {
     try {
-        const track = getTrack(songName, artistName, apiKey);
+        const track = getTrack(songName, artistName, "db1815126935bc7fef98a221fafbf0fe");
 
         // if (track) {
         //     const lyricsUrl = `https://api.musixmatch.com/ws/1.1/track.lyrics.get?track_id=${track.track_id}&apikey=${apiKey}`;
@@ -22,17 +22,40 @@ async function displaySongInfo(songName, artistName) {
         }).catch((error) => {
             console.log(error);
         })
+        
     } catch (error) {
         console.error('Error displaying song info:', error);
         $('#lyrics').text('Error fetching data');
     }
 }
 
-$(document).ready(() => {
-    $('#search-button').click(() => {
-        const songName = $('#song-name').val().trim();
-        const artistName = $('#artist-name').val().trim();
 
+
+
+$(document).ready(() => {
+    // Modal handling
+    console.log('I exists');
+    const $findMusicBtn = $('#findMusic');
+    const $modalBg = $('.modal-background');
+    const $modal = $('.modal');
+
+    
+    $findMusicBtn.on('click', () => {
+        $modal.addClass('is-active');
+    });
+    $modalBg.on('click', () => {
+        $modal.removeClass('is-active');
+    });
+
+    console.log('Still alive');
+    // Search button handling
+    $('#field').on("submit", (e) => {
+        e.preventDefault();
+
+        const songName = $('input[type=text][name=container-songname]').val();
+        const artistName = $('input[type=text][name=container-artistname]').val();
+
+        console.log(songName, "  ", artistName);
         if (songName && artistName) {
             displaySongInfo(songName, artistName);
         } else {
